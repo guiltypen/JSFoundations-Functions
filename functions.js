53ca731ec39f21ed0d27eea0af10d0b0acc41994
Bader Al-Ramadan <bader.ram@gmail.com>
2021-02-02 20:40:26 +0300

    functions yarn says i failed :'(

--- functions.js
+++ functions.js
@@ -1,53 +1,58 @@
-/**
- * greet(name):
- * - receives a name
- * - logs "Hello <name>"
- *
- * e.g.
- * greet("Hamza") logs "Hello Hamza"
- */
+
+// Declaring a variable
+let name = "Bader"
+ let numStatus;
+ let num = 7;
+ let result;
+
+ // greating function
 function greet(name) {
-  // Your code here
+
+  console.log("Hello "+ name)
 }
 
-/**
- * isOdd(n):
- * - receives a number n
- * - returns true if it's odd, false otherwise
- *
- * e.g.
- * isOdd(7) -> true
- * isOdd(10) -> false
- */
+greet(name); // print greeting
+
+// check if odd return true 
 function isOdd(n) {
-  // Your code here
+numStatus = (n % 2 == 0 ) ? false:true
+
 }
 
-/**
- * oddsSmallerThan(n):
- * - receives a number n
- * - returns the number of ODD numbers smaller than n
- *
- * e.g.
- * oddsSmallerThan(7) -> 3
- * oddsSmallerThan(15) -> 7
- */
+isOdd (num);
+console.log("is the number odd? "+ numStatus)
+
+// calcuate how many odds smaller than the number
 function oddsSmallerThan(n) {
-  // Your code here
+ 
+  if (numStatus == true){
+    console.log (`There are   ${(n/2) - 0.5}  Odds smaller than ${num}`)
+  }else
+  console.log (`There are   ${(n/2)}  Odds smaller than ${num}`)
+ 
 }
 
-/**
+oddsSmallerThan(num);
+/*
  * squareOrDouble(n):
  * - receives a number n
  * - returns its square if it's odd
  * - returns its double if it's even
- *
- * e.g.
- * squareOrDouble(16) -> 32
- * squareOrDouble(9) -> 81
  */
+
 function squareOrDouble(n) {
-  // Your code here
+
+  if (numStatus == true){
+    result = n*n
+  }else
+   result = 2*n;
 }
+
+squareOrDouble(num);
+if (numStatus == true) {
+console.log(`since ${num} is odd the square is ${result}`);
+}else {
+
+}console.log(`since ${num} is even the double is ${result}`);
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
